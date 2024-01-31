c414953efe412c8f4bada3f375dbf333d67e3e91
caua-dev-coder <cauatavellaprofissional@gmail.com>
2024-01-31 15:12:24 -0300

    Adicionando MediaQueries

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-let image = document.querySelector('projectImgsLinks');
+
+
 window.sr = ScrollReveal ({reset: true});
 
 sr.reveal(".about-me", {duration: 1500})
 sr.reveal(".skills", {duration: 1500})
 sr.reveal(".projetos", {duration: 1500})
 sr.reveal(".contact", {duration: 1500})
-
-function showMessage() {
-  let message = document.createElement('p');
-  message.innerHTML = 'Clique para ver o projeto !';
-  document.body.appendChild(message);
-}
-
-image.addEventListener('mouseenter', showMessage);
